c266e63f69e9e1bfedb8d51d92e58d1493f43604
[cazan] <cazanmadalin12@gmail.com>
2025-09-23 17:45:44 +0300

    mic save , nu stiu ce fac

---
+++ ITEMS-ANG/src/app/features/items/components/items-table/items-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-items-table',
+  imports: [],
+  templateUrl: './items-table.component.html',
+  styleUrl: './items-table.component.scss'
+})
+export class ItemsTableComponent {
+
+}
---
+++ ITEMS-ANG/src/app/features/items/items-main/items-main.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-items-main',
+  imports: [],
+  templateUrl: './items-main.component.html',
+  styleUrl: './items-main.component.scss'
+})
+export class ItemsMainComponent {
+
+}
